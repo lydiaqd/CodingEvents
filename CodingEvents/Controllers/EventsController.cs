7f1accc9162754be5f8a66473c352ea93c09f934
lydiaqd <75919027+lydiaqd@users.noreply.github.com>
2023-10-25 01:58:24 -0500

    Added Delete Function

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Models;
+using CodingEvents.Data;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,12 +12,12 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-       private static List<string> Events = new List<string>();
 
         // GET: /<controller>/
         public IActionResult Index()
-        {
-            ViewBag.Events = Events;
+        { 
+            ViewBag.events = EventData.GetAll();
+
             return View();
         }
 
@@ -27,9 +29,27 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name)
+        public IActionResult NewEvent(string name, string description )
         {
-            Events.Add(name);
+            EventData.Add(new Event(name, description));
+
+            return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
 
             return Redirect("/Events");
         }
---
+++ CodingEvents/Data/EventData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CodingEvents.Models;
+
+namespace CodingEvents.Data
+{
+	public class EventData
+	{
+		static private Dictionary<int, Event> Events = new Dictionary<int, Event>();
+
+		//Fetch all the events
+		public static IEnumerable<Event> GetAll()
+		{
+			return Events.Values;
+		}
+
+		//Add a new event to dictionary
+		public static void Add(Event newEvent)
+		{
+			Events.Add(newEvent.Id, newEvent);
+		}
+
+		//Remove an event
+		public static void Remove(int id)
+		{
+			Events.Remove(id);
+		}
+
+		//Fetch an event
+		public static Event GetById(int id)
+		{ 
+			return Events[id];
+		}
+	} 
+}
+
